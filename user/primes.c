ca086a33079f2d1c43d11479d665777d78f5fb98
wyl <kofjoker0@gmail.com>
2023-10-05 16:15:33 +0800

    fix the primes.c pipe close bug

--- user/primes.c
+++ user/primes.c
@@ -20,74 +20,75 @@ loop:
 void foo();
 
 int main() {
-    int fd[2];
-    if(pipe(fd) < 0) {
-        printf("pipe error\n");
-        exit(-1);
-    }
+  int fd[2];
+  if (pipe(fd) < 0) {
+    printf("pipe error\n");
+    exit(-1);
+  }
 
-    int pid = fork();
-    if (pid > 0) {
+  int pid = fork();
+  if (pid > 0) {
     // parent process
-        // close pipe's read end;        
-        close(fd[0]);
+    // close pipe's read end;
+    close(fd[0]);
 
-        // write 2-32 to a pipe
-        for (int i = 2; i < 33; i++) {
-            write(fd[1], &i, sizeof(int));
-        }
-
-        close(fd[1]);
-
-        wait(0);
-    } else if(pid == 0) {
-        foo(fd);
+    // write 2-32 to a pipe
+    for (int i = 2; i < 33; i++) {
+      write(fd[1], &i, sizeof(int));
     }
-    exit(0);
+
+    close(fd[1]);
+
+    wait(0);
+  } else if (pid == 0) {
+    foo(fd);
+  }
+  exit(0);
 }
 
 void foo(int *fd_parent) {
-    // close pipe's write end;
-    close(fd_parent[1]);
-    int p = 0;
+  // close pipe's write end;
+  close(fd_parent[1]);
+  int p = 0;
 
-    // p = get a number from left neighbor
-    if (read(fd_parent[0], &p, sizeof(int)) > 0) {
-        // print p
-        printf("prime %d\n", p);
-    } else {
-        exit(0);
-    }
+  // p = get a number from left neighbor
+  if (read(fd_parent[0], &p, sizeof(int)) > 0) {
+    // print p
+    printf("prime %d\n", p);
+  } else {
+    exit(0);
+  }
 
-    // create pipe
-    int fds[2];        
-    if (pipe(fds) < 0) {
-        printf("pipe error\n");
-        exit(-1);
-    }
-        
-    // loop:
-    //     n = get a number from left neighbor
-    //     if (p does not divide n)
-    //         send n to right neighbor
+  // create pipe
+  int fds[2];
+  if (pipe(fds) < 0) {
+    printf("pipe error\n");
+    exit(-1);
+  }
 
-    int n = 0;
-    while (read(fd_parent[0], &n, sizeof(int)) > 0) {
-        if (n % p != 0) {
-            write(fds[1], &n, sizeof(int));
-        }
+  // loop:
+  //     n = get a number from left neighbor
+  //     if (p does not divide n)
+  //         send n to right neighbor
+
+  int n = 0;
+  while (read(fd_parent[0], &n, sizeof(int)) > 0) {
+    if (n % p != 0) {
+      write(fds[1], &n, sizeof(int));
     }
-    
-    int pid = fork();
-    if (pid > 0) {
-        // close read end and write end;
-        close(fds[0]);
-        close(fds[1]);
-        wait(0);
-    } else if (pid == 0) {
-        foo(fds);
-        // close readend, the wrie end was close in foo().
-        close(fds[0]);
-        exit(0);
-    }
+  }
+  close(fd_parent[0]);
+
+  int pid = fork();
+  if (pid > 0) {
+    // close read end and write end;
+    close(fds[0]);
+    close(fds[1]);
+    wait(0);
+  } else if (pid == 0) {
+    foo(fds);
+    // close readend, the wrie end was close in foo().
+    close(fds[0]);
+    exit(0);
+  }
 }
